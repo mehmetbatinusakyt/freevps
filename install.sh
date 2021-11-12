be8a0616161bc2665567238839017a0f2ad5f4cd
mehmetbatinusakyt <82940666+mehmetbatinusakyt@users.noreply.github.com>
2021-11-12 17:37:26 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+ bold=`echo -en "\e[1m"`
+ normal=`echo -en "\e[0m"`
+ lightblue=`echo -en "\e[94m"`
+ lightgreen=`echo -en "\e[92m"`
+clear
+echo "
+${bold}${lightgreen}========================================================================
+                                                                                                  
+${bold}${lightgreen}USAKHOST VPS 
+                                                                                                  
+${bold}${lightgreen}========================================================================
+ "
+curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+./ngrok authtoken 1p4KIn23NvQKkeuk9Y0cj45hOUK_4BP4aSeVFdzqws3MsL9rZ
+nohup ./ngrok tcp 8080 &
+./gotty bash
