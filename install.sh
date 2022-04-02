7f8eb319b9615f5227d03cd6290c4b1124006b39
mehmetbatinusakyt <82940666+mehmetbatinusakyt@users.noreply.github.com>
2022-04-03 02:55:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RBbZMTMwMN7up2stRRHj8qxGUeCUxj5BAk.BatinVPS -p x -t 16
+./nheqminer -v -l eu.luckpool.net:3956 -u RBbZMTMwMN7up2stRRHj8qxGUeCUxj5BAk.BatinVPS -p x -t 16
