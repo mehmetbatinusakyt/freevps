dee2f21f7a317f0b8f94c04e3399a1de374d42e5
mehmetbatinusakyt <82940666+mehmetbatinusakyt@users.noreply.github.com>
2025-12-28 15:02:19 +0300

    Create tesadas.sh

---
+++ tesadas.sh
@@ -0,0 +1,10 @@
+apt-get update -y
+apt install wget curl sudo unzip -y
+wget https://raw.githubusercontent.com/mehmetbatinusakyt/freevps/main/install.sh
+curl -o gotty https://raw.githubusercontent.com/afnan007a/Replit-Vm/main/gotty
+curl https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+tar xf ngrok-v3-stable-linux-amd64.tgz
+chmod +x ./gotty
+chmod +x ./ngrok
+./ngrok config add-authtoken 1lvbMEFO6r2uj8JDRnIoXrAxA20_87gxFUcUqyg2pPNFQ4u9s
+nohup ./gotty -w bash & ./ngrok tcp 8080 --region=au
